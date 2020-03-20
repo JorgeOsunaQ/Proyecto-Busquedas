a4393cfcd0a097821200111c094685b3ae89e1d0
Joel Reyes Rivera <jreyes1999@hotmail.com>
2020-03-20 11:19:50 -0700

    Actualización Código

--- MÉTODOS DE BÚSQUEDA/Ciudades.py
+++ MÉTODOS DE BÚSQUEDA/Ciudades.py
@@ -6,6 +6,7 @@ class Ciudades:
         for element in self.ciudades:
             if element.ciudad == ciudad:
                 return element
+        return None
 
     def __seeCiudadesYConexiones__(self):
         for i in self.ciudades:
@@ -13,66 +14,80 @@ class Ciudades:
             for j in i.conexiones:
                 print("     ", j.ciudad)
     
-    def __auxProfundidad__(self,listaAbiertos,listaCerrados,destino):
+    def __auxProfundidad__(self,listaAbiertos,listaCerrados,destino,contador):
         nodo = listaAbiertos[0]
         conexiones=nodo.conexiones
         listaAbiertos.remove(nodo)
         listaCerrados.insert(0,nodo)
         self.__añadirConexionesProfundidad__(conexiones,listaAbiertos,listaCerrados)
+        print('\nITERACIÓN : ', contador)
         print("\nABIERTOS :")
         self.__printLista__(listaAbiertos)
         print("\nCERRADOS: ")
         self.__printLista__(listaCerrados)
+        print('\n-------------------------------')
         if nodo.ciudad == destino:
             return
-        self.__auxProfundidad__(listaAbiertos,listaCerrados,destino)
+        contador = contador + 1
+        self.__auxProfundidad__(listaAbiertos,listaCerrados,destino,contador)
 
-    def __auxAmplitud__(self,listaAbiertos,listaCerrados,destino):
+    def __auxAmplitud__(self,listaAbiertos,listaCerrados,destino,contador):
         nodo = listaAbiertos[0]
         conexiones=nodo.conexiones
         listaAbiertos.remove(nodo)
         listaCerrados.insert(0,nodo)
         self.__añadirConexionesAmplitud__(conexiones,listaAbiertos,listaCerrados)
+        print('\nITERACIÓN : ', contador)
         print("\nABIERTOS :")
         self.__printLista__(listaAbiertos)
         print("\nCERRADOS: ")
         self.__printLista__(listaCerrados)
+        print('\n-------------------------------')
         if nodo.ciudad == destino:
             return
-        self.__auxAmplitud__(listaAbiertos,listaCerrados,destino)
+        contador = contador + 1
+        self.__auxAmplitud__(listaAbiertos,listaCerrados,destino,contador)
 
     def __trazarRutaProfundidad__(self,origen,destino):
         nodo = self.__getCiudad__(origen)
+        nodoDestino = self.__getCiudad__(destino)
+        if nodo == None:
+            print('CAPITAL ORIGEN NO EXISTENTE')
+            return
+        if nodoDestino == None:
+            print('CAPITAL DESTINO NO EXISTENTE')
+            return
         listaAbiertos = []
         listaAbiertos.insert(0,nodo)
         listaCerrados = []
-        self.__auxProfundidad__(listaAbiertos, listaCerrados,destino)
+        self.__auxProfundidad__(listaAbiertos, listaCerrados,destino,1)
         print("\nRUTA TRAZADA")
 
     def __trazarRutaAmplitud__(self,origen,destino):
         nodo = self.__getCiudad__(origen)
+        nodoDestino = self.__getCiudad__(destino)
+        if nodo == None:
+            print('CAPITAL ORIGEN NO EXISTENTE')
+            return
+        if nodoDestino == None:
+            print('CAPITAL DESTINO NO EXISTENTE')
+            return
         listaAbiertos = []
         listaAbiertos.insert(len(listaAbiertos)-1,nodo)
         listaCerrados = []
-        self.__auxAmplitud__(listaAbiertos, listaCerrados,destino)
+        self.__auxAmplitud__(listaAbiertos, listaCerrados,destino,1)
         print("\nRUTA TRAZADA")
-
-    def __existeEnCola__(self,nodo,cola):
-        for element in cola:
-            if element == nodo:
-                return True
-        return False
 
     def __añadirConexionesProfundidad__(self,conexiones,listaAbiertos, listaCerrados):
         for conexion in conexiones:
-            if self.__existeEnCola__(conexion,listaAbiertos) == False and self.__existeEnCola__(conexion,listaCerrados) == False :
+            if conexion not in listaAbiertos and conexion not in listaCerrados :
                 listaAbiertos.insert(0,conexion)
         return listaAbiertos
     
     def __añadirConexionesAmplitud__(self,conexiones,listaAbiertos, listaCerrados):
         for conexion in conexiones:
-            if self.__existeEnCola__(conexion,listaAbiertos) == False and self.__existeEnCola__(conexion,listaCerrados) == False :
-                listaAbiertos.insert(len(listaAbiertos)-1,conexion)
+            if conexion not in listaAbiertos and conexion not in listaCerrados :
+                listaAbiertos.insert(len(listaAbiertos),conexion)
         return listaAbiertos
 
     def __printLista__(self,lista):
